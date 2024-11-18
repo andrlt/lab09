ae8b46a6152667b7824bcc7c19c180799981abd4
Andrea La Tosa <andrea.latosa@studio.unibo.it>
2024-11-18 13:04:05 +0100

    Finish Controller

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+import javax.swing.JOptionPane;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
 
+    private final File file;
+
+    public Controller(){
+        this.file = new File("output.txt");
+    }
+
+    public Controller(final File file, final String path) {
+        this.file = file;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+    public String getPathFile(){
+        return this.file.getAbsolutePath();
+    }
+
+    public void writeFile(String s) throws IOException{
+        try(PrintStream ps = new PrintStream(getPathFile(), StandardCharsets.UTF_8)){
+            ps.print(s);
+        }
+    }
 }
